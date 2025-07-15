f10d3f0e7e3d3f127324f361be983d2004184ba8
vishal <mgvishalctr@gmail.com>
2025-07-15 22:27:43 +0530

    push

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  });
+  function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.style.display = navLinks.style.display === "block" ? "none" : "block";
+  }
+
+  document.querySelectorAll('.animate').forEach(el => observer.observe(el));
+  
